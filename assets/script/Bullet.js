cffaea103e373079bd807d77c0764cc938dc35cd
SuperK1989 <15196636616@163.com>
2018-12-01 16:48:47 +0800

    更新

--- assets/script/Bullet.js
+++ assets/script/Bullet.js
@@ -3,16 +3,36 @@ cc.Class({
     extends: cc.Component,
 
     properties: {
-        
+
+        speed: 5,
+        damage: 10,
+        position: (1000, 1000),
     },
 
-    // LIFE-CYCLE CALLBACKS:
+    onLoad() {
+        this.wLimit = cc.view.getVisibleSize().width;
+        this.hLimit = cc.view.getVisibleSize().height;
+        this.node.setPosition(0, 0);
+        console.log(this.node.getPosition())
+    },
 
-    // onLoad () {},
-
-    start () {
+    start() {
 
     },
 
-    // update (dt) {},
+    init(gamePlay) {
+        console.log(this.node.getPosition())
+        let action = cc.moveBy(0.1, 0, 1000);
+        this.node.runAction(action);
+        this.node.scale = 2;
+        this.gameplay = gamePlay;
+    },
+
+    update(dt) {
+        if (this.node.y > this.wLimit / 2) {
+            this.gameplay.putBulletToPoll(this.node);
+            this.node.setPosition(0, 0);
+            this.node.removeFromParent();
+        }
+    },
 });
--- assets/script/Gameplay.js
+++ assets/script/Gameplay.js
@@ -1,30 +1,53 @@
-// Learn cc.Class:
-//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/class.html
-//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/class.html
-// Learn Attribute:
-//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
-//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/reference/attributes.html
-// Learn life-cycle callbacks:
-//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
-//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/life-cycle-callbacks.html
-
+var addAction = require('ObjAni')
 cc.Class({
     extends: cc.Component,
 
     properties: {
-        NodePlayer:cc.Node,
-        NodeEnemy:cc.Node,
+        prefabBullet: cc.Prefab,
+        bulletPool: cc.NodePool,
+        parentNode: cc.Node,
     },
 
     // LIFE-CYCLE CALLBACKS:
 
-    onLoad () {
+    onLoad() {
+        this.setBulletPool();
 
     },
 
-    start () {
+    start() {
 
     },
 
-    // update (dt) {},
+    openFire() {
+        console.log(this.parentNode.children);
+        let bullet = null;
+        if (this.bulletPool.size() > 0) { // 通过 size 接口判断对象池中是否有空闲的对象
+            bullet = this.bulletPool.get();
+        } else { // 如果没有空闲对象，也就是对象池中备用对象不够时，我们就用 cc.instantiate 重新创建
+            bullet = cc.instantiate(this.enemyPrefab);
+        }
+        bullet.parent = this.parentNode; // 将生成的敌人加入节点树
+        bullet.getComponent('Bullet').init(this); //接下来就可以调用 enemy 身上的脚本进行初始化
+        console.log(this.parentNode.children);
+    },
+
+    setBulletPool() {
+        this.bulletPool = new cc.NodePool();
+        let initCount = 5;
+        for (let i = 0; i < initCount; ++i) {
+            let bullet = cc.instantiate(this.prefabBullet); // 创建节点
+            this.bulletPool.put(bullet); // 通过 putInPool 接口放入对象池
+        }
+    },
+
+    putBulletToPoll(node) {
+        this.bulletPool.put(node);
+    },
+
+
+
+    update(dt) {
+
+    },
 });
